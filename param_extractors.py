ef76c9a9c10c653328315f9569bccb1f8a580dc0
jwigmore <jwigmore@mit.edu>
2023-03-28 13:51:02 -0500

    removed `parse_jsons()` and updated `parse_training_json()` for training json parameters based on a single environment

--- param_extractors.py
+++ param_extractors.py
@@ -4,23 +4,7 @@ import json
 class Args(object):
     pass
 
-def parse_json(json_path: str):
 
-    para = json.load(open(json_path))
-    args = Args()
-    test_args = Args()
-    env_para = para["problem_instance"]
-    algo_para = para["algo_parameters"]
-    test_para = para["test_parameters"]
-
-    for key, value in algo_para.items():
-        setattr(args, key, value)
-    args.batch_size = int(args.num_envs * args.num_steps)
-    args.minibatch_size = int(args.batch_size // args.num_minibatches)
-
-    for key, value in test_para.items():
-        setattr(test_args, key, value)
-    return env_para, args, test_args
 
 def parse_jsons(training_json_path, env_json_path, test_json_path):
     config_args = Args()
@@ -37,7 +21,8 @@ def parse_training_json(json_path, config_args):
     for key, value in algo_para.items():
         setattr(args, key, value)
         setattr(config_args, f"train/{key}", value)
-
+    args.num_steps = args.num_steps_per_env/args.num_envs
+    args.reset_steps = args.reset_steps_per_env/args.num_envs
     args.batch_size = int(args.num_envs * args.num_steps)
     args.minibatch_size = int(args.batch_size // args.num_minibatches)
     args.num_resets = args.total_timesteps /args.reset_steps # total number of new trajectories seen
