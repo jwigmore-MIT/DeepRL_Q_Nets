ce55f685d8271fcded172b99b519c4725077167c
jwigmore-MIT <jwigmore@mit.edu>
2023-07-20 10:50:06 -0400

    Added requirements.txt

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='DeepRL_Q_Nets',
+    version='',
+    packages=['SB3', 'SB3.config', 'CORL', 'safety', 'Environments', 'd3rlpy_runner', 'NonDRLPolicies'],
+    url='',
+    license='',
+    author='jwigm',
+    author_email='',
+    description=''
+)
