2a1dd38cf8e87aeea11535ba6337d1f71a65739c
jwigmore-laptop <jwigmore@mit.edu>
2023-08-18 11:42:03 -0500

    Added DP for value iteration

---
+++ DP/tabular_policy.py
@@ -0,0 +1,16 @@
+import random
+from collections import defaultdict
+from policy import DeterministicPolicy
+
+
+class TabularPolicy(DeterministicPolicy):
+    def __init__(self, default_action=None):
+        self.policy_table = defaultdict(lambda: default_action)
+
+    def select_action(self, state):
+        if not isinstance(state, tuple):
+            state = tuple(state)
+        return self.policy_table[state]
+
+    def update(self, state, action):
+        self.policy_table[state] = action
